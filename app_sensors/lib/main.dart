ac8ba33cd441a8ee48fb003c34e2fbe036e5d629
Bryan Vargas <bryanvrgsc@gmail.com>
2023-04-04 20:05:46 -0600

    project commit, receiving data from phone

--- app_sensors/lib/main.dart
+++ app_sensors/lib/main.dart
@@ -1,115 +1,242 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:core';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_sensors/flutter_sensors.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _accelAvailable = false;
+  bool _gyroAvailable = false;
+  List<double> _accelData = List.filled(3, 0.0);
+  List<double> _gyroData = List.filled(3, 0.0);
+  StreamSubscription? _accelSubscription;
+  StreamSubscription? _gyroSubscription;
+  final StreamController<String> _dataStreamController =
+      StreamController<String>();
+
+  @override
+  void initState() {
+    _checkAccelerometerStatus();
+    _checkGyroscopeStatus();
+    super.initState();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void dispose() {
+    _stopAccelerometer();
+    _stopGyroscope();
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _checkAccelerometerStatus() async {
+    await SensorManager()
+        .isSensorAvailable(Sensors.ACCELEROMETER)
+        .then((result) {
+      setState(() {
+        _accelAvailable = result;
+      });
     });
   }
 
+  Future<void> _startAccelerometer() async {
+    if (_accelSubscription != null) return;
+    if (_accelAvailable) {
+      final stream = await SensorManager().sensorUpdates(
+        sensorId: Sensors.ACCELEROMETER,
+        interval: Sensors.SENSOR_DELAY_FASTEST,
+      );
+      _accelSubscription = stream.listen((sensorEvent) {
+        setState(() {
+          _accelData = sensorEvent.data;
+        });
+      });
+    }
+  }
+
+  void _stopAccelerometer() {
+    if (_accelSubscription == null) return;
+    _accelSubscription?.cancel();
+    _accelSubscription = null;
+  }
+
+  void _checkGyroscopeStatus() async {
+    await SensorManager().isSensorAvailable(Sensors.GYROSCOPE).then((result) {
+      setState(() {
+        _gyroAvailable = result;
+      });
+    });
+  }
+
+  void startSendingData() async {
+    final socket = await Socket.connect('localhost', 8090);
+    _dataStreamController.stream.listen((data) {
+      socket.write(json.encode({'data': data}));
+      socket.flush();
+    });
+    Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      _dataStreamController.add(json.encode({
+        'accelX': _accelData[0],
+        'accelY': _accelData[1],
+        'accelZ': _accelData[2],
+        'gyroX': _gyroData[0],
+        'gyroY': _gyroData[1],
+        'gyroZ': _gyroData[2]
+      }));
+    });
+  }
+
+  void stopSendingData() {
+    _dataStreamController.close();
+  }
+
+  Future<void> _startGyroscope() async {
+    if (_gyroSubscription != null) return;
+    if (_gyroAvailable) {
+      final stream =
+          await SensorManager().sensorUpdates(sensorId: Sensors.GYROSCOPE);
+      _gyroSubscription = stream.listen((sensorEvent) {
+        setState(() {
+          _gyroData = sensorEvent.data;
+        });
+      });
+    }
+  }
+
+  void _stopGyroscope() {
+    if (_gyroSubscription == null) return;
+    _gyroSubscription?.cancel();
+    _gyroSubscription = null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sensors'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          alignment: AlignmentDirectional.topCenter,
+          child: Column(
+            children: <Widget>[
+              const Text(
+                "Accelerometer Test",
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Accelerometer Enabled: $_accelAvailable",
+                textAlign: TextAlign.center,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[0](X) = ${_accelData[0]}",
+                textAlign: TextAlign.center,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[1](Y) = ${_accelData[1]}",
+                textAlign: TextAlign.center,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[2](Z) = ${_accelData[2]}",
+                textAlign: TextAlign.center,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  MaterialButton(
+                    color: Colors.green,
+                    onPressed:
+                        _accelAvailable ? () => _startAccelerometer() : null,
+                    child: const Text("Start"),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  MaterialButton(
+                    color: Colors.red,
+                    onPressed:
+                        _accelAvailable ? () => _stopAccelerometer() : null,
+                    child: const Text("Stop"),
+                  ),
+                ],
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              const Text(
+                "Gyroscope Test",
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Gyroscope Enabled: $_gyroAvailable",
+                textAlign: TextAlign.center,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[0](X) = ${_gyroData[0]}",
+                textAlign: TextAlign.center,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[1](Y) = ${_gyroData[1]}",
+                textAlign: TextAlign.center,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[2](Z) = ${_gyroData[2]}",
+                textAlign: TextAlign.center,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  MaterialButton(
+                    color: Colors.green,
+                    onPressed: _gyroAvailable ? () => _startGyroscope() : null,
+                    child: const Text("Start"),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  MaterialButton(
+                    color: Colors.red,
+                    onPressed: _gyroAvailable ? () => _stopGyroscope() : null,
+                    child: const Text("Stop"),
+                  ),
+                ],
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              MaterialButton(
+                color: Colors.green,
+                onPressed: () => startSendingData(),
+                child: const Text("Send Data"),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 16.0)),
+              MaterialButton(
+                color: Colors.red,
+                onPressed: () => stopSendingData(),
+                child: const Text("Stop Data"),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
